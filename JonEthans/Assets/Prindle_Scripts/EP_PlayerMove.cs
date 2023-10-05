46357bc77a8f1c4cd145f4b1a18c366d998030d0
Ethan P <63009250+Ebrawl@users.noreply.github.com>
2023-10-05 00:45:10 -0400

    player Rotate with mouse

--- JonEthans/Assets/Prindle_Scripts/EP_PlayerMove.cs
+++ JonEthans/Assets/Prindle_Scripts/EP_PlayerMove.cs
@@ -7,14 +7,20 @@ public class EP_PlayerMove : MonoBehaviour
     // Start is called before the first frame update
 
     public float speed;
-    public float damage =5;
-    public float health = 20;
+    public float damage =5f;
+    public float health = 20f;
+
+    private Vector2 mousePos;
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical"); 
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        float angle = Mathf.Atan2(mousePos.y -transform.position.y, mousePos.x - transform.position.x) *Mathf.Rad2Deg -90f;
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
         transform.position += new Vector3(h,v,0f) *speed *Time.deltaTime;
     }
     private void OnCollisionEnter2D(Collision2D other)
