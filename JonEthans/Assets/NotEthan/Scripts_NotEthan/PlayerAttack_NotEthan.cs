5c07570ce73a5ec46b7036c86d642690c4b5d8b1
Jon Miller <Jwmiller2002@gmail.com>
2023-10-07 04:34:19 -0400

    Player Attack Animation

---
+++ JonEthans/Assets/NotEthan/Scripts_NotEthan/PlayerAttack_NotEthan.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerAttack_NotEthan : MonoBehaviour
+{
+
+    [SerializeField] private Animator anim;
+
+    [SerializeField] private float meleeSpeed;
+
+    [SerializeField] private float damage;
+
+    float timeUntiMelee;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeUntiMelee <= 0f)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Attack");
+                timeUntiMelee = meleeSpeed;
+            }
+        }
+        else
+        {
+            timeUntiMelee -= Time.deltaTime;
+        }
+    }
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "enemy") 
+        {
+            //other.GetComponent<enemy_NotEthan>().takeDamage(damage);
+            Debug.Log("Hit");
+        }
+    }
+}
