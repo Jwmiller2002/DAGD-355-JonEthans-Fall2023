52411676b2a7e9049b01e69a6609fda6129f74a7
Ethan P <63009250+Ebrawl@users.noreply.github.com>
2023-10-08 21:32:15 -0400

    Death is working
    made gameover motions and stopped player movement

--- JonEthans/Assets/Prindle_Scripts/EP_PlayerMove.cs
+++ JonEthans/Assets/Prindle_Scripts/EP_PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,20 +14,30 @@ public class EP_PlayerMove : MonoBehaviour
     private Vector2 mousePos;
     public GameObject player;
     public GameObject weapon;
+    private Boolean isDead =false;
+    
     // Update is called once per frame
     void Update()
     {
+       
+       
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical"); 
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         float angle = Mathf.Atan2(mousePos.y -transform.position.y, mousePos.x - transform.position.x) *Mathf.Rad2Deg -360f;
         //float hammerAngle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg + 222f;
-
-        transform.localRotation = Quaternion.Euler(0, 0, angle);
-        //weapon.transform.localRotation = Quaternion.Euler(0, 0, hammerAngle);
-        oldpos = transform.position;
-        transform.position += new Vector3(h,v,0f) *speed *Time.deltaTime;
+        if (isDead == false)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, angle);
+            //weapon.transform.localRotation = Quaternion.Euler(0, 0, hammerAngle);
+            oldpos = transform.position;
+            transform.position += new Vector3(h, v, 0f) * speed * Time.deltaTime;
+        }
+        else
+        {
+            print("GAME OVER");
+        }
         if(oldpos != transform.position)
         {
             anim.SetBool("runing", true);
@@ -42,7 +53,7 @@ public class EP_PlayerMove : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Bullet"))
+        if (other.gameObject.CompareTag("Bullet") && isDead == false)
         {
             
             Destroy(other.gameObject);
@@ -51,7 +62,9 @@ public class EP_PlayerMove : MonoBehaviour
             anim.SetTrigger("hit");
             if (health <= 0)
             {
-                anim.SetBool("death", true);
+                anim.SetTrigger("Dead");
+                anim.SetBool("isDead",true);
+                isDead = true;
                 //Destroy(gameObject);
                 
             }
