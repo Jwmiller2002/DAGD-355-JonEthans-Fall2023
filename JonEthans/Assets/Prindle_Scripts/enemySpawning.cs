2770323e774ed15cf463afe4a1a8072089b6d0fd
Jon Miller <Jwmiller2002@gmail.com>
2023-10-18 22:26:39 -0400

    Spawner Fix

--- JonEthans/Assets/Prindle_Scripts/enemySpawning.cs
+++ JonEthans/Assets/Prindle_Scripts/enemySpawning.cs
@@ -7,11 +7,17 @@ public class enemySpawning : MonoBehaviour
     
     public GameObject basicEnemy;
     public GameObject rocketEnemy;
+    public GameObject strongEnemy;
+    public GameObject supportEnemy;
     public GameObject spawner;
     [SerializeField] private float basicSpawnTimerMax;
     [SerializeField] private float rocketSpawnTimerMax;
-    private float basicSpawnTimer = 2f;
-    private float rocketSpawnTimer = 0f;
+    [SerializeField] private float supportEnemyTimerMax;
+    [SerializeField] private float strongEnemyTimerMax;
+    private float basicSpawnTimer = 0f;
+    private float rocketSpawnTimer = 10f;
+    private float supportEnemyTimer = 10f;
+    private float strongEnemyTimer = 5f;
     
     [SerializeField] private float MaxEnemies =1f;
     [SerializeField] private float MaxEnemiesrocketMan=2f;
@@ -23,7 +29,7 @@ public class enemySpawning : MonoBehaviour
     {
         if (basicSpawnTimer <= 0 && numBasicEnemies <= MaxEnemies)
         {
-            basicSpawnTimerMax = Random.Range(3, 8);
+            basicSpawnTimerMax = Random.Range(1, 5);
             basicSpawnTimer = basicSpawnTimerMax;
             //print("SPAWNED");
             Vector3 spawnlocation = spawner.transform.position;
@@ -38,8 +44,8 @@ public class enemySpawning : MonoBehaviour
         }
         if (rocketSpawnTimer < 0 && numRocketEnemies < MaxEnemiesrocketMan)
         {
-            print("what");
-            rocketSpawnTimerMax = Random.Range(3, 8);
+            //print("what");
+            rocketSpawnTimerMax = Random.Range(10, 15);
             rocketSpawnTimer = rocketSpawnTimerMax;
             //print("SPAWNED");
             Vector3 spawnlocation = spawner.transform.position;
@@ -51,9 +57,36 @@ public class enemySpawning : MonoBehaviour
         else
         {
             rocketSpawnTimer -= Time.deltaTime;
-            print(rocketSpawnTimer);
+            //print(rocketSpawnTimer);
         }
-        if(difficultyIncrease <= 0)
+        if (supportEnemyTimer <= 0)
+        {
+            supportEnemyTimerMax = Random.Range(10, 15);
+            supportEnemyTimer = supportEnemyTimerMax;
+            Vector3 spawnlocation = spawner.transform.position;
+            Quaternion spawnRotation = spawner.transform.rotation;
+            Instantiate(supportEnemy, spawnlocation, spawnRotation);
+
+        }
+        else
+        {
+            supportEnemyTimer -= Time.deltaTime;
+        }
+        if (strongEnemyTimer <= 0)
+        {
+            strongEnemyTimerMax = Random.Range(5, 10);
+            strongEnemyTimer = strongEnemyTimerMax;
+            Vector3 spawnlocation = spawner.transform.position;
+            Quaternion spawnRotation = spawner.transform.rotation;
+            Instantiate(strongEnemy, spawnlocation, spawnRotation);
+
+        }
+        else
+        {
+            strongEnemyTimer -= Time.deltaTime;
+        }
+
+        if (difficultyIncrease <= 0)
         {
             MaxEnemies += 5;
             MaxEnemiesrocketMan += 2;
