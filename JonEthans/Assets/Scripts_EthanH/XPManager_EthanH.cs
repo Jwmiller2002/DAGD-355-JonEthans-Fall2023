6561e965ae998b4be8a11b8f9e30e9df51672fca
Jon Miller <Jwmiller2002@gmail.com>
2023-10-18 23:55:35 -0400

    XP Fix

--- JonEthans/Assets/Scripts_EthanH/XPManager_EthanH.cs
+++ JonEthans/Assets/Scripts_EthanH/XPManager_EthanH.cs
@@ -8,11 +8,12 @@ public class XPManager_EthanH : MonoBehaviour
 
     public Image xpBar;
     public float xpAmount;
+    public float nextLevelAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        nextLevelAmount = 10;
     }
 
     // Update is called once per frame
@@ -23,9 +24,15 @@ public class XPManager_EthanH : MonoBehaviour
     public void GainXP(float xpGain)
     {
         xpAmount += xpGain;
-        xpAmount = Mathf.Clamp(xpAmount, 0, 10);
+        xpAmount = Mathf.Clamp(xpAmount, 0, nextLevelAmount);
 
         xpBar.fillAmount = xpAmount / 10f;
+    }
+
+    public void ResetBar()
+    {
+        xpAmount = 0;
+        nextLevelAmount = nextLevelAmount * 2;
     }
 
 }
